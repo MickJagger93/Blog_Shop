296e1b34841df2c5199d1e52f97d5e602ceed00d
JaggerMQ <quevedo993mi@gmail.com>
2026-05-02 10:06:08 +0530

    Modificando superusuario

--- website/create_admin.py
+++ website/create_admin.py
@@ -7,12 +7,19 @@ django.setup()
 from django.contrib.auth import get_user_model
 
 User = get_user_model()
-username = 'Mick93' 
+username = 'Jagger93'
 email = 'guillen993mi@gmail.com'
-password = 'Test44%%' 
+password = 'Test44%%'
 
-if not User.objects.filter(username=username).exists():
+try:
+    
+    User.objects.filter(email='Mick93').delete()
+    User.objects.filter(username=username).delete()
+    User.objects.filter(email=email).delete()
+    
     User.objects.create_superuser(username, email, password)
-    print(f"Superusuario '{username}' creado exitosamente.")
-else:
-    print(f"El superusuario '{username}' ya existe.")
+    print(f"✅ ÉXITO: Superusuario '{username}' creado desde cero.")
+
+except Exception as e:
+    print(f"⚠️ Nota: {e}")
+
