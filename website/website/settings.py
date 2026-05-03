3f3e32a4f9801612986017950170a8b8e9aeb0a0
JaggerMQ <quevedo993mi@gmail.com>
2026-05-03 06:23:53 +0530

    DEBUG = True

--- website/website/settings.py
+++ website/website/settings.py
@@ -26,7 +26,7 @@ load_dotenv(os.path.join(BASE_DIR, '.env'))
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', 'False') == 'True'
+DEBUG = True
 
 ALLOWED_HOSTS = ['jaggershop.onrender.com', '127.0.0.1', 'localhost']
 CSRF_TRUSTED_ORIGINS = ['https://onrender.com']
