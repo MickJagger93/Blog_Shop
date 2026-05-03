fcc18a2857325a0ab5ecb0c9736b49f7fa23f95e
JaggerMQ <quevedo993mi@gmail.com>
2026-05-04 03:29:46 +0530

    Eliminando archivo widgets y modificando admin.py

--- website/shop/admin.py
+++ website/shop/admin.py
@@ -1,41 +1,16 @@
 from django.contrib import admin
-from import_export import resources, fields
-from import_export.widgets import ForeignKeyWidget
-from import_export.admin import ImportExportModelAdmin, ExportMixin
+from import_export import resources
+from import_export.admin import ExportMixin
 from .models import Category, Product, UserActivity
-from .widgets import CloudinaryImageWidget
-from django.utils.text import slugify
 
-class ProductResource(resources.ModelResource):
-    
-    category = fields.Field(
-        column_name='category',
-        attribute='category',
-        
-        widget=ForeignKeyWidget(Category, 'name')
-    )
-
-    image = fields.Field(
-        column_name='image',
-        attribute='image',
-        widget=CloudinaryImageWidget()
-    )
-
-    class Meta:
-        model = Product
-        fields = ('id', 'name', 'category', 'description', 'price', 'stock', 'is_active', 'image')
-        import_id_fields = ('id',)
-
-    def before_import_row(self, row, **kwargs):
-        
-        if 'slug' not in row or not row['slug']:
-            if 'name' in row and row['name']:
-                row['slug'] = slugify(row['name'])
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('name', 'slug')  
+    prepopulated_fields = {'slug': ('name',)}
 
 @admin.register(Product)
-class ProductAdmin(ImportExportModelAdmin):
-    resource_class = ProductResource 
-    list_display = ('name', 'category', 'is_active')  
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('name', 'category', 'price', 'stock', 'is_active')  
     list_filter = ('category', 'is_active')
     search_fields = ('name',)
     prepopulated_fields = {'slug': ('name',)}
@@ -52,8 +27,9 @@ class UserActivityAdmin(ExportMixin, admin.ModelAdmin):
     list_display = ('created_at', 'user', 'event_type', 'description', 'ip_address')
     list_filter = ('event_type', 'created_at')
     search_fields = ('description', 'user__username')
-    readonly_fields = ('created_at',) 
+    readonly_fields = ('created_at',)
 
     def get_event_display(self, obj):
         return obj.get_event_type_display()
     get_event_display.short_description = 'Tipo de Evento'
+
